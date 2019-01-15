bf59bc7981ae1c6a990344ff6668f77ec3c74d4b
William Lin <williamlinsfms@gmail.com>
2019-01-15 12:12:49 -0800

    First commit

--- app/src/main/java/com/example/william/mqttskeleton/MainActivity.java
+++ app/src/main/java/com/example/william/mqttskeleton/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -19,12 +20,17 @@ public class MainActivity extends AppCompatActivity {
     SeekBar bar;
 
     boolean on = false;
+    //The default progress
+    int prog = 50;
+    //The SoundMeter, which is a class in the package
+    SoundMeter meter;
 
-    String topic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        meter = new SoundMeter();
 
         setContentView(R.layout.activity_main);
 
@@ -46,11 +52,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
+                prog = progress;
                 if(progress <=60 && progress >= 40){
+                    //Esablishes eadzone from (40-60)
                     Log.d("progress bar", "to the stopped");
                     byte[] send = {0};
                     mqttHelper.publish(send, "Stop");
                 }else if(progress <=40){
+                    //Checks if progress bar is less than 40, (deadzone from 40-60)
                     Log.d("progress bar", "to the to the left");
                     int count = 40-progress;
                     byte[] send = new byte[5];
@@ -66,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("print toSend", Arrays.toString(send));
                     mqttHelper.publish(send, "Left");
                 }else if(progress >=60){
+                    //Checks if progress bar is greater than 60, (deadzone from 40-60)
                     Log.d("progress bar", "to the right");
                     int count = progress-60;
                     byte[] send = new byte[5];
@@ -87,25 +97,77 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         startMqtt();
+        meter.start();
 
 
-
+        //Starts a separate thread listening for loud noises constantly
+        new Thread(new Runnable () {
+            public void run(){
+                while(true){
+                    double x = meter.getAmplitude();
+                    Log.d("AmplitudeValue", String.valueOf(x));
+                    if(x > 31000) {
+                        byte[] send = {0, 0, 0, 0, 0};
+                        mqttHelper.publish(send, "Clap");
+                    }
+                    try {
+                        Thread.sleep(75);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        meter.start();
+        //Restarts recording the sound
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        meter.stop();
+        //Stops recording the sound
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        meter.release();
+    }
     public void Off(View view){
         on = false;
-        byte[] send = {0};
-        mqttHelper.publish(send, "Left");
+        byte[] send = {0,0,0,0,0};
+        mqttHelper.publish(send, "Off");
+        bar.setProgress(50);
+        //Sends a message to turn the thing off
     }
 
     public void On(View view){
         on = true;
-        byte[] send = {0};
-        mqttHelper.publish(send, "Right");
+        byte[] send = {0,0,0,0,0};
+        mqttHelper.publish(send, "On");
+        //TODO: Check if this triggers the "On Progress Changed"
+        //If it does, no problem. If it doesn't, change back to like 0 and then prog
+        int currentProg = prog;
+        //Quick reset for on/off, sets it to off then starts
+        bar.setProgress(50);
+        bar.setProgress(currentProg);
     }
 
 
+    //WARNING: This will exit the python code running on the Pi, so will need to reboot pi to restart.
+    public void Exit(View view){
+        byte[] send = {0,0,0,0,0};
+        mqttHelper.publish(send, "Exit");
+    }
+
     private void startMqtt(){
+        //Starts MQTT, will run on onCreate anyways
         mqttHelper = new MqttHelper(getApplicationContext());
         mqttHelper.setCallback(new MqttCallbackExtended() {
             @Override
---
+++ app/src/main/java/com/example/william/mqttskeleton/SoundMeter.java
@@ -0,0 +1,52 @@
+package com.example.william.mqttskeleton;
+
+import android.media.MediaRecorder;
+import android.util.Log;
+
+public class SoundMeter {
+
+    private MediaRecorder mRecorder = null;
+
+    //NOTE! Ensure that permission for microphone is enabled, otherwise will crash
+
+    public void start() {
+        if (mRecorder == null) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+            try {
+                mRecorder.prepare();
+                }catch(Exception e){
+                e.printStackTrace();
+            }
+            mRecorder.start();
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public void release() {
+        if (mRecorder != null){
+            mRecorder.release();
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  mRecorder.getMaxAmplitude();
+        else
+            return 0;
+
+    }
+
+
+
+}
